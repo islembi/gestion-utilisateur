34716c357245e9a13e3bffd54640129c50f3f0a5
islem <islem.benothmen.27@gmail.com>
2021-11-21 22:41:23 +0100

    formulaire + ajout d'un utilisateur + delete button

--- gestion.js
+++ gestion.js
@@ -9,10 +9,58 @@ var list = document.getElementById('users');
 
 function addUser(){
     var username = document.getElementById('username').value;
+    var leprenom = document.getElementById('leprenom').value;
     var email = document.getElementById('email').value;
-    var entry = document.createElement('li');
-    entry.appendChild(document.createTextNode(username + ' ' + email));
-    list.appendChild(entry);
-    
-    return false;
+    var laposte = document.getElementById('laposte').value;
+    var tbodyRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+    var rowCount = $("#table tr").length;
+// insertion ligne a la fin de table
+var newRow = tbodyRef.insertRow();
+
+// donner id a la ligne
+newRow.id = "tr-"+rowCount;
+
+// creation boutton
+let btn = document.createElement("button");
+
+// creation fonction delete avec le id  
+btn.innerHTML = "Delete";
+
+btn.onclick = function () {
+  var row = document.getElementById("tr-"+rowCount);
+    row.parentNode.removeChild(row);
+};
+
+// Append a nom
+  var newNom = document.createTextNode(username);
+  var newCell = newRow.insertCell();
+  newCell.appendChild(newNom);
+// Append a nom
+  var newPrenom = document.createTextNode(leprenom);
+  var newCell = newRow.insertCell();
+  newCell.appendChild(newPrenom);
+// Append a nom
+  var newEmail = document.createTextNode(email);
+  var newCell = newRow.insertCell();
+  newCell.appendChild(newEmail);
+ // Append a nom
+  var newlaposte = document.createTextNode(laposte);
+  var newCell = newRow.insertCell();
+  newCell.appendChild(newlaposte);
+ // Append  buton
+  var newCell = newRow.insertCell();
+  newCell.appendChild(btn);
+  return false;
 }
+
+// Reset form après la fermeture 
+function reset(){
+    document.getElementById('username').value = "";
+    document.getElementById('leprenom').value = "";
+    document.getElementById('email').value = ""; 
+    document.getElementById('laposte').value = ""; 
+}
+
+
+
+
