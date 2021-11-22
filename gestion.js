fd7af9998c0c2eed102fced87e5ad34ea31b6a6d
islem <islem.benothmen.27@gmail.com>
2021-11-22 18:22:48 +0100

    ajout de page todolist

--- gestion.js
+++ gestion.js
@@ -55,10 +55,52 @@ btn.onclick = function () {
 
 // Reset form après la fermeture 
 function reset(){
-    document.getElementById('username').value = "";
-    document.getElementById('leprenom').value = "";
-    document.getElementById('email').value = ""; 
-    document.getElementById('laposte').value = ""; 
+    document.getElementById('tache').value = "";
+}
+
+
+
+
+//ajout de tache
+var malist = document.getElementById('todo');
+
+
+function addTache(){
+    var tache = document.getElementById('tache').value;
+    var tbodyRef = document.getElementById('table').getElementsByTagName('tbody')[0];
+    var rowCount = $("#table tr").length;
+// insertion ligne a la fin de table
+var newRow = tbodyRef.insertRow();
+
+// donner id a la ligne
+newRow.id = "tr-"+rowCount;
+
+// creation boutton
+let btn = document.createElement("button");
+
+// creation fonction delete avec le id  
+btn.innerHTML = "Delete";
+
+btn.onclick = function () {
+  var row = document.getElementById("tr-"+rowCount);
+    row.parentNode.removeChild(row);
+};
+
+// Append a tache
+  var newNom = document.createTextNode(tache);
+  var newCell = newRow.insertCell();
+  newCell.appendChild(newNom);
+
+
+ // Append  buton
+  var newCell = newRow.insertCell();
+  newCell.appendChild(btn);
+  return false;
+}
+
+// Reset form après la fermeture 
+function reset(){
+    document.getElementById('tache').value = "";
 }
 
 
